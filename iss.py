f5cabafbcd197d0e2ab664b387fa471681de6579
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2013-01-23 07:25:30 -0800

    155631.607201

--- iss.py
+++ iss.py
@@ -2,16 +2,22 @@
 
 from math import cos, sin, acos, asin, atan, pi
 import math
-import json
-import sys
 
 # Parameters are determined in function of the beta angle.
 # There is a fixed list of beta angles so we can over-optimize for that...
+
+"""
+other insteresting possible starts:
+{"72": {"backpanels_angle": -0.058081170108740809, "frontpanels_angle": 0.096524828897619264, "fast_cycle_2_accel": 0.3131072637717327, "yaw": 0.0, "fast_cycle_1_start": 2.191132510985939, "fast_cycle_1_length": 0.64123387684647237, "fast_cycle_1_accel": 0.13249386279639749, "fast_cycle_2_length": 1.6653094751964459, "fast_cycle_2_start": 4.9912847588147446}
+[152382.1368978579, {"74": {"backpanels_angle": 0.0, "frontpanels_angle": 0.34000000000000002, "fast_cycle_2_accel": 0.31, "yaw": 0.0, "fast_cycle_1_start": 3.0699999999999998, "fast_cycle_1_length": 1.74, "fast_cycle_1_accel": 0.31, "fast_cycle_2_length": 2.0899999999999999, "fast_cycle_2_start": 4.9299999999999997}}]
+[158538.77428211164, {"-70": {"backpanels_angle": -0.058081170108740809, "frontpanels_angle": 0.096524828897619264, "fast_cycle_2_accel": 0.3131072637717327, "yaw": 0.0, "fast_cycle_1_start": 2.191132510985939, "fast_cycle_1_length": 0.64123387684647237, "fast_cycle_1_accel": 0.13249386279639749, "fast_cycle_2_length": 1.6653094751964459, "fast_cycle_2_start": 4.9912847588147446}}]
+
+"""
 PARAMS = {
-  "72": {"backpanels_angle": -0.081096612144675229, "frontpanels_angle": 0.13477408593500995, "fast_cycle_2_accel": 0.3131072637717327, "yaw": 0.0, "fast_cycle_1_start": 2.191132510985939, "fast_cycle_1_length": 0.64123387684647237, "fast_cycle_1_accel": 0.13249386279639749, "fast_cycle_2_length": 1.6653094751964459, "fast_cycle_2_start": 4.9912847588147446},
+  "72": {"backpanels_angle": 0.0033042337152939865, "frontpanels_angle": -0.01056640892439243, "fast_cycle_2_accel": 0.3428089319992873, "yaw": 0.0, "fast_cycle_1_start": 3.0776485207915103, "fast_cycle_1_length": 1.7427536927878089, "fast_cycle_1_accel": 0.31596510086613522, "fast_cycle_2_length": 2.0889489709868618, "fast_cycle_2_start": 4.926173498557965},
   "74": {"backpanels_angle": 0.0, "frontpanels_angle": 0.33000000000000002, "fast_cycle_2_accel": 0.31, "yaw": 0.0, "fast_cycle_1_start": 3.0699999999999998, "fast_cycle_1_length": 1.74, "fast_cycle_1_accel": 0.31, "fast_cycle_2_length": 2.0899999999999999, "fast_cycle_2_start": 4.9299999999999997},
-  "-70": {"backpanels_angle": -0.081096612144675229, "frontpanels_angle": 0.13477408593500995, "fast_cycle_2_accel": 0.3131072637717327, "yaw": 0.0, "fast_cycle_1_start": 2.191132510985939, "fast_cycle_1_length": 0.64123387684647237, "fast_cycle_1_accel": 0.13249386279639749, "fast_cycle_2_length": 1.6653094751964459, "fast_cycle_2_start": 4.9912847588147446},
-  "-74": {"backpanels_angle": 0.17439514350525442, "frontpanels_angle": -0.28738534366121887, "fast_cycle_2_accel": 0.20999999999999999, "yaw": 0.0, "fast_cycle_1_start": 3.0699999999999998, "fast_cycle_1_length": 1.74, "fast_cycle_1_accel": 0.14999999999999999, "fast_cycle_2_length": 2.0899999999999999, "fast_cycle_2_start": 4.9299999999999997},
+  "-70": {"backpanels_angle": 0.00077547121336952074, "frontpanels_angle": 0.0080927148776482015, "fast_cycle_2_accel": 0.27364128093748397, "yaw": 0.0, "fast_cycle_1_start": 3.0739091675605743, "fast_cycle_1_length": 1.7409763869535488, "fast_cycle_1_accel": 0.30972196648796291, "fast_cycle_2_length": 2.2001081667798479, "fast_cycle_2_start": 4.8524435650820354},
+  "-74": {"backpanels_angle": 0.17224776864608016, "frontpanels_angle": -0.29483245917231549, "fast_cycle_2_accel": 0.20905563634834368, "yaw": 0.0, "fast_cycle_1_start": 3.0699986441940013, "fast_cycle_1_length": 1.7399720037085906, "fast_cycle_1_accel": 0.1497756507810214, "fast_cycle_2_length": 2.0898894192387836, "fast_cycle_2_start": 4.9299050494815209},
 
 
   #example
@@ -26,17 +32,7 @@ PARAMS = {
     "fast_cycle_2_length": 2.09,
     "fast_cycle_2_accel": 0.21
   },
-  "70": {
-    "yaw": 0,
-    "frontpanels_angle": 0.4,
-    "backpanels_angle": 0,
-    "fast_cycle_1_start": 3.07,
-    "fast_cycle_1_length": 1.74,
-    "fast_cycle_1_accel": 0.15,
-    "fast_cycle_2_start": 4.93,
-    "fast_cycle_2_length": 2.09,
-    "fast_cycle_2_accel": 0.21
-  },
+  "70": {"backpanels_angle": -0.15463899577456608, "frontpanels_angle": -0.031156437841873164, "fast_cycle_2_accel": 0.15097816777464132, "yaw": 0.0, "fast_cycle_1_start": 3.8116338629615298, "fast_cycle_1_length": 2.2348404440689351, "fast_cycle_1_accel": 0.09516664073371689, "fast_cycle_2_length": 2.9033926538153909, "fast_cycle_2_start": 5.6560269696997105},
 
   # competition
   "75": {
@@ -66,15 +62,15 @@ def posdegrees(grad):
 # returns alpha, beta
 # TODO fix this crap, doesn't work w/ yaw
 def vect2angle(vect):
-  beta = asin(-vect[1])
+  beta = asin(min(1, max(-1, -vect[1])))
 
-  alpha = acos(-vect[2] / cos(beta))
+  alpha = acos(min(1, max(-1, -vect[2] / cos(beta))))
   if vect[2]<0 and vect[0]>0:
-    alpha = asin(vect[0] / cos(beta))
+    alpha = asin(min(1, max(-1, vect[0] / cos(beta))))
   if vect[2]>0 and vect[0] < 0:
-    alpha = -acos(-vect[2] / cos(beta))
+    alpha = -acos(min(1, max(-1, -vect[2] / cos(beta))))
   if vect[2]<0 and vect[0] < 0:
-    alpha = -acos(-vect[2] / cos(beta))
+    alpha = -acos(min(1, max(-1, -vect[2] / cos(beta))))
 
   return [alpha, beta]
 
