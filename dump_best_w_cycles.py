4d11fd44f022d2cf554cf4ce1c5a6f95bd26bf4f
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2013-01-31 02:45:59 -0500

    155398.784744

--- dump_best_w_cycles.py
+++ dump_best_w_cycles.py
@@ -24,8 +24,11 @@ params = {}
 for p in ranges:
   beta = p.split(" ")[0]
   if not beta in params:
-    params[beta] = {}
-  params[beta].update(ranges[p])
+    params[beta] = ranges[p]
+  for i in range(int(p.split(" ")[1].split("-")[0]),1+int(p.split(" ")[1].split("-")[1])):
+    k = "sarjd_%s" % i
+    if k in ranges[p]:
+      params[beta][k] = ranges[p][k]
 
 print "avg : %s" % (avg*1.0/cnt)
 print
