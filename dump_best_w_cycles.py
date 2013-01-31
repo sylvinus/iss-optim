82197e5478028c051ccaf1b53bace0e473c9d856
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2013-01-31 16:12:30 -0500

    156721.832334

--- dump_best_w_cycles.py
+++ dump_best_w_cycles.py
@@ -12,7 +12,9 @@ avg = 0
 cnt = 0
 res = DB.results.find().sort("score", -1)
 for r in res:
-  k = r["beta"]+" "+r["range"]
+  k = r["beta"]
+  if "-" in r["range"]:
+    k+=" "+r["range"]
   if not k in ranges:
     cnt += 1
     avg += r["score"]
@@ -25,10 +27,11 @@ for p in ranges:
   beta = p.split(" ")[0]
   if not beta in params:
     params[beta] = ranges[p]
-  for i in range(int(p.split(" ")[1].split("-")[0]),1+int(p.split(" ")[1].split("-")[1])):
-    k = "sarjd_%s" % i
-    if k in ranges[p]:
-      params[beta][k] = ranges[p][k]
+  if " " in p:
+    for i in range(int(p.split(" ")[1].split("-")[0]),1+int(p.split(" ")[1].split("-")[1])):
+      k = "sarjd_%s" % i
+      if k in ranges[p]:
+        params[beta][k] = ranges[p][k]
 
 print "avg : %s" % (avg*1.0/cnt)
 print
